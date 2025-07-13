b1b2213a033c83a81f60e43783aab04c99177dac
“Bytebanditt” <wiwawinnie@gmail.com>
2025-07-13 17:39:19 +0300

    Added sentence concatenation script

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+// Get the arguments passed to the script
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print the output in the required format
+console.log(firstArg + " is " + secondArg);
